e7de326cc9946a8fe5793fa139da6b25f250fc6b
palooney <lixx3013@umn.edu>
2017-03-30 23:59:20 -0500

    log in page rewrite

--- app/src/main/java/umn/umncalendar/LoginActivity.java
+++ app/src/main/java/umn/umncalendar/LoginActivity.java
@@ -1,8 +1,12 @@
 package umn.umncalendar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,19 +16,37 @@ public class LoginActivity extends AppCompatActivity {
     private EditText emailTag;
     private EditText passwordTag;
     private Button signinBtn;
+    private TextView signupBtn;
 
-
-    @Override
-    public void onCreate(Button savedInstanceState){
+    public void OnCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-    }
 
-    public void LoginButton(){
-        emailTag = (EditText) findViewById(R.id.input_email);
-        passwordTag = (EditText) findViewById(R.id.input_password);
-        signinBtn = (Button) findViewById(R.id.btn_login)
-    }
+        // define clicks on the log in page
+        emailTag = (EditText)findViewById(R.id.input_email);
+        passwordTag = (EditText)findViewById(R.id.input_password);
+        signinBtn = (Button)findViewById(R.id.btn_login);
+        signupBtn = (TextView)findViewById(R.id.liunk_signup);
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        }); // new View.OnClickListener()
+        signupBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                // start with new sign-up activity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+            }
+        });
+
+    }// onCreate()
+
+
 
 }
