b9c6ccd5a85292f97fb9e9428069907411c38fda
palooney <lixx3013@umn.edu>
2017-03-31 00:06:10 -0500

    log in page update

--- app/src/main/java/umn/umncalendar/LoginActivity.java
+++ app/src/main/java/umn/umncalendar/LoginActivity.java
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 login();
             }
-        }); // new View.OnClickListener()
+        }); // new View.OnClickListener() for signinBtn
         signupBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View V){
@@ -42,10 +42,15 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
                 finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
-        });
+        }); // new View.OnClickListener() for signupBtn
 
     }// onCreate()
+
+    public void login(){
+
+    }// login
 
 
 
