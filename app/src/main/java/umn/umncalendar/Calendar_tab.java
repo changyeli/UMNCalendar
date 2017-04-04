147be1192ac08c5fd5cf0e493aa0ec4c7f08852b
Anshi <anshi31jagrawal@gmail.com>
2017-04-04 15:02:50 -0500

    Calendar view tab fragment code

--- app/src/main/java/umn/umncalendar/Calendar_tab.java
+++ app/src/main/java/umn/umncalendar/Calendar_tab.java
@@ -1,20 +1,56 @@
 package umn.umncalendar;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /**
- * Created by AartiRajan on 4/4/2017.
+ * Created by COMP1 on 04-04-2017.
  */
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class Calendar_tab extends Fragment {
-    @Nullable
+import java.util.Calendar;
+
+public class Calendar_tab extends AppCompatActivity {
+
+    EditText date;
+    DatePickerDialog datePickerDialog;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar_tab, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_calendar_tab);
+        // initiate the date picker and a button
+        date = (EditText) findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(Calendar_tab.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
     }
+
+
 }
+
