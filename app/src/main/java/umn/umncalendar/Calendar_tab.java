44705b3ec535bc2660eeb56244857b3bdeb00a70
aartirajan <aartirajan@gmail.com>
2017-04-07 19:47:50 -0500

    Anushree - Reset filters, Event Detail Page

--- app/src/main/java/umn/umncalendar/Calendar_tab.java
+++ app/src/main/java/umn/umncalendar/Calendar_tab.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -25,16 +27,29 @@ import java.util.List;
  */
 
 public class Calendar_tab extends Fragment implements AdapterView.OnItemSelectedListener{
+    View currView;
     TextView test;
     TextView date;
     DatePickerDialog datePickerDialog;
     ImageButton datePicker;
+    Spinner dropdown;
+    CheckBox freeFoodchk;
+    CheckBox freeEntryChk;
+    CheckBox onCampusChk;
+    Button reset;
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View currView = inflater.inflate(R.layout.fragment_calendar_tab, container, false);
-        Spinner dropdown = (Spinner)(currView.findViewById(R.id.filters));
+        currView = inflater.inflate(R.layout.fragment_calendar_tab, container, false);
+        date = (TextView) (currView.findViewById(R.id.date));
+        datePicker = (ImageButton)(currView.findViewById(R.id.datePicker));
+        dropdown = (Spinner)(currView.findViewById(R.id.filters));
+        freeFoodchk=(CheckBox)(currView.findViewById(R.id.free_food_chk));
+        freeEntryChk=(CheckBox)(currView.findViewById(R.id.free_entry_chk));
+        onCampusChk=(CheckBox)(currView.findViewById(R.id.on_campus_chk));
+        reset=(Button) (currView.findViewById(R.id.reset_filters_btn));
+
         dropdown.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter
                 .createFromResource(getContext(), R.array.filters_list,
@@ -47,28 +62,26 @@ public class Calendar_tab extends Fragment implements AdapterView.OnItemSelected
         // Apply the adapter to the spinner
         dropdown.setAdapter(adapter);
 
-        date = (TextView) (currView.findViewById(R.id.date));
-        datePicker = (ImageButton)(currView.findViewById(R.id.datePicker));
-        // calender class's instance and get current date , month and year from calender
-        final Calendar c = Calendar.getInstance();
-        int mYear = c.get(Calendar.YEAR); // current year
-        int mMonth = c.get(Calendar.MONTH); // current month
-        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-        // date picker dialog
-        date.setText(mDay + "/"
-                + (mMonth + 1) + "/" + mYear);
+        setTodayDate();
         datePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getFragmentManager(), "datePicker");
-            }
-        });
+                                    @Override
+                                    public void onClick(View v){
+                                        DialogFragment newFragment = new DatePickerFragment();
+                                        newFragment.show(getFragmentManager(), "datePicker");
+                                    }
+                                });
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getFragmentManager(), "datePicker");
+                newFragment.show(getFragmentManager(), "datePi" +
+                        "cker");
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                resetFilters();
             }
         });
 
@@ -90,4 +103,25 @@ public class Calendar_tab extends Fragment implements AdapterView.OnItemSelected
         /*test = (TextView)(getView().findViewById(R.id.test));
         test.setText(parent.getItemAtPosition(0).toString());*/
     }
+
+    public void setTodayDate(){
+        // calender class's instance and get current date , month and year from calender
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        // date picker dialog
+        date.setText(mDay + "/"
+                + (mMonth + 1) + "/" + mYear);
+    }
+
+    public void resetFilters(){
+        setTodayDate();
+        dropdown.setSelection(0);
+        freeFoodchk.setChecked(false);
+        freeEntryChk.setChecked(false);
+        onCampusChk.setChecked(false);
+    }
+
+
 }
--- app/src/main/java/umn/umncalendar/RsvpCancelDialog.java
+++ app/src/main/java/umn/umncalendar/RsvpCancelDialog.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,7 +39,8 @@ public class RsvpCancelDialog extends DialogFragment implements View.OnClickList
     @Override
     public void onClick(View v) {
         if(v.getId()==R.id.btn_yes){
-
+            Fragment f = getParentFragment();
+            System.out.println(f.getId());
         }
         if(v.getId()==R.id.btn_no){
             dismiss();
