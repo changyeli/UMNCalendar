5bd8e22da1f9caa61ec5c9f056a43a39ba22282d
Jia Zhong <zhong240@umn.edu>
2017-04-09 16:51:13 -0500

    added search button

---
+++ README.md
@@ -0,0 +1,42 @@
+# UMNCalendar
+
+## Introduction
+
+We are a group of students who are studying User Interface Design under Dr. Lana Yarosh. As part of this course project, my team and I are designing a Shared University Calendar for the students at the University of Minnesota. Our team’s goal is to make the experience of finding relevant University events to students easier and more pleasant. Currently, event calendars are scattered, and some student organizations have obscure pages and a limited way to inform students of their events. Mailing lists are difficult to get onto, most students don’t have sufficient information about an event, which results in them usually missing events of their interest. We hope to use your input to make this app one that students will enjoy using. 
+
+## Motivation
+- Information overload is a big problem with students. Students are bombarded with posters and e-mails
+- At the same time, there’s too little information about events that are actually relevant to them.
+
+### Goal
+
+- Find interesting events quickly!
+  - Free food: feeling hungry?
+  - Timing: does it fit your schedule?
+  - Location: find what's within your reach.
+  - Interests: meed with people who share your hobbies.
+  - Career: career fairs and on-campus company visits.
+- See what your friends are up to.
+  - See if your friends are going to the same event.
+  - Invite your friends.
+  - Carpool with your friends with your friends to event.
+  - Reunite with old friends and meet with new friends.
+  - Share updates of your interests with your friends.
+  - Add new friends!
+- Personalized events
+  - Get event recommendations that are tailored to your interest.
+  - Each user has a unique profile.
+  - Checks your event schedule for any event clashes.
+- Stay up-to-date and never miss an event again.
+  - Event reminders
+  - Location change notificaitons
+- For host: 
+  - Upload your event posters.
+  - Share event details in one place.
+  - Connect with your guests in advance.
+  - Get an expected turnout.
+
+## About Us
+
+We are a team of 6 students, all attending the University of Minnesota: Anushree Jagrawal, Aarti Sundararajan, Changye Li, Chris Fan, Jia Zhong, and Dianna Nguyen. This study is being conducted as a part of Dr. Lana Yarosh’s CSCI 5115 course.
+
---
+++ app/src/main/java/umn/umncalendar/MyEventTabAdapter.java
@@ -0,0 +1,112 @@
+package umn.umncalendar;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by AartiRajan on 4/4/2017.
+ */
+
+public class MyEventTabAdapter extends ArrayAdapter<Event> {
+    // List<Event> eventList;
+    Event currEvent;
+    public MyEventTabAdapter(@NonNull Context context, List<Event> events) {
+        super(context, R.layout.custom_row_myevent, events);
+        //eventList = events;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View customView = layoutInflater.inflate(R.layout.custom_row_myevent, parent, false);
+        final Event singleEvent =  getItem(position);
+        TextView name = (TextView)customView.findViewById(R.id.eventname);
+        ImageView poster = (ImageView)customView.findViewById(R.id.eventposter);
+        TextView category = (TextView)customView.findViewById(R.id.eventcategory);
+        TextView date = (TextView)customView.findViewById(R.id.eventdate);
+        TextView location = (TextView)customView.findViewById(R.id.eventlocation);
+        LinearLayout freeFood = (LinearLayout) customView.findViewById(R.id.free_food_indicator);
+        name.setText(singleEvent.getEventName());
+        poster.setImageResource(singleEvent.getPoster());
+        category.setText(singleEvent.getCategory().name());
+        date.setText(singleEvent.getFormattedDate());
+        location.setText(singleEvent.getAddress());
+
+        if(!singleEvent.isFood()){
+            freeFood.setVisibility(View.GONE);
+        }
+        setCancelRsvpListener(customView, singleEvent);
+
+        return  customView;
+    }
+
+
+    public void setCancelRsvpListener(View customView, final Event event){
+        Button cancelRsvp = (Button)customView.findViewById(R.id.cancelRsvp);
+        cancelRsvp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setCancelable(true);
+                builder.setTitle("Are you sure you want to cancel this RSVP?");
+                builder.setMessage("This event will be removed from your events, and you will no longer receive updates for this event");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // How to remove the selected item?
+                        EventManager em = new EventManager();
+                        em.myevents.remove(event);
+                        notifyDataSetChanged();
+                    }
+
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                /*FragmentActivity activity = (FragmentActivity)getContext();
+                FragmentManager fm = activity.getSupportFragmentManager();
+                RsvpCancelDialog alertDialog = new RsvpCancelDialog();
+                alertDialog.setEvent(singleEvent);
+                alertDialog.show(fm, "fragment_alert");*/
+
+
+            }
+        });
+    }
+
+}
+
+
