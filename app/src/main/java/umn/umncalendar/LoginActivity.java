2d7c885a0075f3ee19e35a9009e5a57e45c5f291
palooney <lixx3013@umn.edu>
2017-04-10 18:38:23 -0500

    layout update

--- app/src/main/java/umn/umncalendar/LoginActivity.java
+++ app/src/main/java/umn/umncalendar/LoginActivity.java
@@ -49,21 +49,21 @@ public class LoginActivity extends AppCompatActivity {
         // create dummy user event interest database for this app
         // Changye
         ArrayList<String> interest_changye = new ArrayList<>();
-        interest_changye.add("Music");
+        interest_changye.add("Photography");
         interest_changye.add("Movie");
-        interest_changye.add("Game");
+        interest_changye.add("Social");
         interest_changye.add("Free Food");
         itHelper.addEntry("lixx3013@umn.edu", interest_changye);
         // Chris
         ArrayList<String> interest_chris = new ArrayList<>();
-        interest_chris.add("Game");
+        interest_chris.add("Academics");
         interest_chris.add("Sport");
         interest_chris.add("Recreation");
         interest_chris.add("Social");
         itHelper.addEntry("fanxx332@umn.edu", interest_chris);
         // James
         ArrayList<String> interest_james = new ArrayList<>();
-        interest_james.add("Talk");
+        interest_james.add("Movie");
         interest_james.add("Academics");
         interest_james.add("Free Food");
         interest_james.add("Sport");
@@ -73,19 +73,19 @@ public class LoginActivity extends AppCompatActivity {
         interest_seth.add("Music");
         interest_seth.add("Sport");
         interest_seth.add("Free Food");
-        interest_seth.add("Game");
+        interest_seth.add("Social");
         itHelper.addEntry("silv2991@umn.edu", interest_seth);
         // Thomas
         ArrayList<String> interest_thomas = new ArrayList<>();
         interest_thomas.add("Academics");
-        interest_thomas.add("Game");
+        interest_thomas.add("Photography");
         interest_thomas.add("Social");
         interest_thomas.add("Recreation");
         itHelper.addEntry("neyx0019@umn.edu", interest_thomas);
         // Jia
         ArrayList<String> interest_jia = new ArrayList<>();
-        interest_jia.add("Talk");
-        interest_jia.add("Music");
+        interest_jia.add("Movie");
+        interest_jia.add("Photography");
         interest_jia.add("Free Food");
         interest_jia.add("Recreation");
         itHelper.addEntry("zhong240@umn.edu", interest_jia);
@@ -93,26 +93,26 @@ public class LoginActivity extends AppCompatActivity {
         ArrayList<String> interest_aarti = new ArrayList<>();
         interest_aarti.add("Music");
         interest_aarti.add("Movie");
-        interest_aarti.add("Talk");
+        interest_aarti.add("Social");
         interest_aarti.add("Academics");
         itHelper.addEntry("sunda134@umn.edu", interest_aarti);
         // Anushree
         ArrayList<String> interest_anushree = new ArrayList<>();
         interest_anushree.add("Academics");
         interest_anushree.add("Sport");
-        interest_anushree.add("Talk");
+        interest_anushree.add("Recreation");
         interest_anushree.add("Movie");
         itHelper.addEntry("jagra001@umn.edu", interest_anushree);
         // Dianna
         ArrayList<String> interest_dianna = new ArrayList<>();
         interest_dianna.add("Movie");
         interest_dianna.add("Music");
-        interest_dianna.add("Talk");
+        interest_dianna.add("Recreation");
         interest_dianna.add("Free Food");
         itHelper.addEntry("nguy2152@umn.edu", interest_dianna);
         // David
         ArrayList<String> interest_david = new ArrayList<>();
-        interest_david.add("Socia");
+        interest_david.add("Social");
         interest_david.add("Sport");
         interest_david.add("Photography");
         interest_david.add("Free Food");
