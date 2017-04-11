491e13f3b3caf15284a3859e9edb2d96749a51ff
aartirajan <aartirajan@gmail.com>
2017-04-10 23:54:28 -0500

    Dianna's changes

--- app/src/main/java/umn/umncalendar/Profile.java
+++ app/src/main/java/umn/umncalendar/Profile.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 
 
 public class Profile extends Fragment {
@@ -15,6 +18,22 @@ public class Profile extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        InterestHelper ih = new InterestHelper();
+        ArrayList<String> interestList = ih.getInterests("gilm7783@umn.edu");
+
+
+        String[] interestArray = {"Sports", "Culture", "Movies"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, interestArray);
+
+        ListView listView = (ListView)v.findViewById(R.id.interests_container);
+        listView.setAdapter(arrayAdapter);
+
+
+        return v;
     }
 }
+
